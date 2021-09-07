ce8228f366715708dd31f6ea9dc7457bda528e07
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2021-09-07 18:35:29 +0200

    init files | added: main, game, display | added: Thread, init BufferStrategy & Graphics

---
+++ src/main/Game.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game implements Runnable {
+	
+	private Display display;
+	private String title;
+	private int width, height;
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	private BufferStrategy bs;
+	private Graphics g;
+
+	public Game(String title, int width, int height) {
+		this.title = title;
+		this.width = width;
+		this.height = height;
+	}
+
+//	=========================================================================	
+	protected synchronized void start() {
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+//	=========================================================================
+	@Override
+	public void run() {
+		init();
+		
+		while(running) {
+			tick();
+			render();
+		}
+		
+		stop();
+	}
+	
+//	=========================================================================
+	public void init() {
+		display = new Display(title, width, height);
+	}
+
+//	=========================================================================
+	public void tick() {
+		
+	}
+	
+//	=========================================================================
+	public void render() {
+		bs = display.getCanvas().getBufferStrategy();
+		
+		if(bs == null) {
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
+		
+		g = bs.getDrawGraphics();
+		g.clearRect(0, 0, width, height);
+		
+		bs.show();
+		g.dispose();
+	}
+	
+//	=========================================================================
+	protected synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game game = new Game("AWT Game", 640, 480);
+		game.start();
+	}
+
+}
